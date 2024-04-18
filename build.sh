39ff96f7e8e918cb6773bd44afb1b6ea97d9683e
CasterraDev <casterradev@gmail.com>
2024-04-18 04:24:32 -0400

    Comments

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 hasBear=$(command -v bear);
 prefix=""
 
+# Some systems (mine) need a program called Bear to generate a compile_commands.json file so that autocomplete, autoimport and linting work correctly.
+# Since not everyone needs this or has Bear installed this is my way of making a build script that will work for both parties.
 if [ ! -z $hasBear ];
 then
     prefix="bear --";
