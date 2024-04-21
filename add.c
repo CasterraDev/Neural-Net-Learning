a4d5b6a9b18b64275ec0ce7ede2fbd1690e974b0
CasterraDev <casterradev@gmail.com>
2024-04-21 04:17:19 -0400

    Added SDL visualization for the cost

--- add.c
+++ add.c
@@ -1,3 +1,6 @@
+#include <SDL2/SDL_rect.h>
+#include <SDL2/SDL_timer.h>
+#include <SDL2/SDL_video.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -6,9 +9,84 @@
 
 #define BITS 4
 
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_render.h>
+#include <stdio.h>
+
+#include <SDL2/SDL.h>
+
+#define WINDOW_FACTOR 80
+#define WINDOW_HEIGHT 9*WINDOW_FACTOR
+#define WINDOW_WIDTH 16*WINDOW_FACTOR
+
+typedef struct {
+    float* items;
+    size_t count;
+    size_t capacity;
+} Plot;
+
+#define plotAppend(p, item)                                                    \
+    do {                                                                       \
+        if ((p)->count >= (p)->capacity) {                                     \
+            (p)->capacity = (p)->capacity == 0 ? 256 : (p)->capacity * 2;      \
+            (p)->items =                                                       \
+                realloc((p)->items, (p)->capacity * sizeof(*(p)->items));      \
+        }                                                                      \
+        (p)->items[(p)->count++] = (item);                                     \
+    } while (0)
+
+void plotCost(SDL_Renderer* renderer, Plot plot, int rx, int ry, int rw,
+              int rh) {
+    SDL_Rect rect;
+    rect.h = rh;
+    rect.w = rw;
+    rect.x = rx;
+    rect.y = ry;
+    SDL_RenderDrawRect(renderer, &rect);
+    float min, max;
+    min = FLT_MAX;
+    max = FLT_MIN;
+    for (size_t i = 0; i < plot.count; ++i) {
+        if (max < plot.items[i])
+            max = plot.items[i];
+        if (min > plot.items[i])
+            min = plot.items[i];
+    }
+    if (min > 0) min = 0;
+    size_t n = plot.count;
+    if (n < 1000)
+        n = 1000;
+    SDL_FPoint points[plot.count];
+    for (size_t i = 0; i + 1 < plot.count; ++i) {
+        float x1 = rx + (float)rw / n * i;
+        float y1 = ry + (1 - (plot.items[i] - min) / (max - min)) * rh;
+        float x2 = rx + (float)rw / n * (i + 1);
+        float y2 = ry + (1 - (plot.items[i + 1] - min) / (max - min)) * rh;
+        SDL_FPoint t;
+        t.x = x1;
+        t.y = y1;
+        SDL_FPoint t2;
+        t2.x = x2;
+        t2.y = y2;
+        points[i] = t;
+        points[i + 1] = t2;
+    }
+    SDL_RenderDrawLinesF(renderer, points, plot.count);
+}
+
 int main(void) {
     srand(time(0));
     // srand(40);
+
+    char running = 1;
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("Error: %s", SDL_GetError());
+    }
+    SDL_Window* window = SDL_CreateWindow("Test Window", 0, 0, 16 * 80, 9 * 80,
+                                          SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(
+        window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    SDL_Event event;
 
     // Generate the training data
     size_t n = (1 << BITS);
@@ -47,41 +125,62 @@ int main(void) {
 
     float rate = 1;
     // REMEMBER: runsAmt is a thing to tweak if nn isn't working completely
-    size_t runsAmt = 100 * 2000;
-    char last = 0;
+    size_t runsAmt = 2000;
+    size_t runs = 0;
     float cost = 0;
-    for (size_t i = 0; i < runsAmt; ++i) {
-        size_t curBatch = i % batchCnt;
-        size_t size = batchSize;
-        size_t batchStart = curBatch * batchSize;
-        // If on the last batch. The size most likely won't be a full batch, so
-        // we fix the size and say it's the last batch of the trainingData so we
-        // can calculate the avg cost.
-        if (td.rows - batchStart < batchSize) {
-            size = td.rows - batchStart;
-            last = 1;
+    Plot plot = {0};
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = 0;
+            }
         }
 
-        // Split the training input/output
-        Mat ti = {.rows = size,
-                  .cols = BITS * 2,
-                  .stride = td.stride,
-                  .es = &MAT_AT(td, batchStart, 0)};
-        Mat to = {.rows = size,
-                  .cols = 1 + BITS,
-                  .stride = td.stride,
-                  .es = &MAT_AT(td, batchStart, ti.cols)};
+        int rw, rh, rx, ry;
+        int padding = 25;
+        rw = (WINDOW_WIDTH / 2) - padding;
+        rh = WINDOW_HEIGHT*2 / 3;
+        rx = padding;
+        ry = (WINDOW_HEIGHT / 2 - rh / 2);
 
-        // Train this batch
-        nnBackprop(nn, g, ti, to);
-        nnLearn(nn, g, rate);
-        cost += nnCost(nn, ti, to);
+        SDL_SetRenderDrawColor(renderer, 18, 18, 18, 255);
+        SDL_RenderClear(renderer);
 
-        if (last) {
-            printf("Average Cost = %f\n", cost / batchSize);
-            cost = 0;
-            last = 0;
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        for (size_t i = 0; i < batchCnt && runs < runsAmt; ++i) {
+            size_t size = batchSize;
+            size_t batchStart = i * batchSize;
+            // If on the last batch. The size most likely won't be a full
+            // batch, so we fix the size and say it's the last batch of the
+            // trainingData so we can calculate the avg cost.
+            if (td.rows - batchStart < batchSize) {
+                size = td.rows - batchStart;
+            }
+
+            // Split the training input/output
+            Mat ti = {.rows = size,
+                      .cols = BITS * 2,
+                      .stride = td.stride,
+                      .es = &MAT_AT(td, batchStart, 0)};
+            Mat to = {.rows = size,
+                      .cols = 1 + BITS,
+                      .stride = td.stride,
+                      .es = &MAT_AT(td, batchStart, ti.cols)};
+
+            // Train this batch
+            nnBackprop(nn, g, ti, to);
+            nnLearn(nn, g, rate);
+            cost += nnCost(nn, ti, to);
+
+            if (i == batchCnt - 1) {
+                printf("%zu: Average Cost = %f\n", i, cost / batchSize);
+                plotAppend(&plot, cost / batchSize);
+                cost = 0;
+            }
         }
+        plotCost(renderer, plot, rx, ry, rw, rh);
+        SDL_RenderPresent(renderer);
     }
 
     NN_PRINT(nn);
@@ -121,6 +220,9 @@ int main(void) {
     printf("--------------------------\n");
     printf("Fails: %zu\n", fails);
     printf("Overflows: %zu\n", overflows);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
--- build.sh
+++ build.sh
@@ -11,4 +11,4 @@ then
 fi;
 
 $prefix clang -Wall -Wextra -o xor xor.c -lm
-$prefix clang -Wall -Wextra -o add add.c -lm
+$prefix clang -Wall -Wextra -o add add.c -lm -I./include/SDL2 -L./lib -lSDL2main -lSDL2
