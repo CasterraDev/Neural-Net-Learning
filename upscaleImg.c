3e7195c0d28e8f6cf9ff42b724b55409cdad3702
CasterraDev <casterradev@gmail.com>
2024-04-24 02:47:11 -0400

    Now Creates upscaled img and save it to FS

--- upscaleImg.c
+++ upscaleImg.c
@@ -12,6 +12,8 @@
 #include "include/nn.h"
 #include "include/nnFile.h"
 #include "include/plot.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "include/stb_image_write.h"
 
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL_render.h>
@@ -69,7 +71,7 @@ int main(int argc, char* argv[]) {
     nnRand(nn, -1, 1);
 
     // REMEMBER: runsAmt is a thing to tweak if nn isn't working completely
-    size_t runsAmt = 1000;
+    size_t runsAmt = 500;
     size_t runs = 0;
     Plot plot = {0};
     size_t scale = 200;
@@ -125,9 +127,8 @@ int main(int argc, char* argv[]) {
     TTF_Font* font = TTF_OpenFont("fonts/HackNerdFont-Regular.ttf", 72);
     SDL_Rect costDestRect;
 
-    SDL_Texture* liveTexture =
-        SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,
-                SDL_TEXTUREACCESS_STREAMING, w, h);
+    SDL_Texture* liveTexture = SDL_CreateTexture(
+        renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, w, h);
     uint8_t* livePixels;
     int livePitch;
 
@@ -179,7 +180,8 @@ int main(int argc, char* argv[]) {
                     nnForward(nn);
                     uint8_t pixel = MAT_AT(NN_OUTPUT(nn), 0, 0) * 255.0f;
 
-                    SDL_LockTexture(liveTexture, NULL, (void**)&livePixels, &livePitch);
+                    SDL_LockTexture(liveTexture, NULL, (void**)&livePixels,
+                                    &livePitch);
 
                     livePixels[(x * 4) + (y * livePitch)] = pixel;
                     livePixels[(x * 4) + (y * livePitch) + 1] = pixel;
@@ -193,6 +195,28 @@ int main(int argc, char* argv[]) {
         }
 
         SDL_RenderPresent(renderer);
+    }
+
+    // Actually upscale the image
+    int outWidth = 256, outHeight = 256;
+    uint8_t* outPixels = malloc(sizeof(uint8_t*) * outWidth * outHeight);
+    for (int x = 0; x < outWidth; x++) {
+        for (int y = 0; y < outHeight; y++) {
+            MAT_AT(NN_INPUT(nn), 0, 0) = (float)x / (outWidth - 1);
+            MAT_AT(NN_INPUT(nn), 0, 1) = (float)y / (outHeight - 1);
+            nnForward(nn);
+            uint8_t pixel = MAT_AT(NN_OUTPUT(nn), 0, 0) * 255.0f;
+
+            outPixels[y * outWidth + x] = pixel;
+        }
+    }
+
+    // Write the image to the FS with stb
+    const char* outFile = "img.png";
+    if (!stbi_write_png(outFile, outWidth, outHeight, 1, outPixels,
+                        sizeof(*outPixels)*outWidth)) {
+        fprintf(stderr, "ERROR: Couldn't make image\n");
+        return 1;
     }
 
     NN_PRINT(nn);
